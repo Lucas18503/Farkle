6859b89c9be50c6bf8872a4a727a601826f9a04c
lucas <lucasbbrown@hotmail.com>
2019-12-06 14:05:55 -0500

    Add files.

---
+++ game.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+void flush_buffer()
+{
+	while((getchar())!='\n');
+}
+int random_number(int lower, int upper)
+{
+	return (rand() % (upper-lower+1))+lower;
+}
+int roll_die()
+{
+	return random_number(1,6);
+}
+struct player
+{
+char *name;
+int score;
+char winmark;
+};
+struct turn
+{
+struct player player;
+	char dice[6];
+	char num_remaining_dice;
+	int score;
+	
+};
+char execute_turn(struct turn trn)
+{
+	//Construct menu.
+	printf("C: check remaining dice.\n");
+	//Check for input.
+	printf("Enter the letter or number of your selection.\n");
+	char selection;
+	scanf("%c",&selection);
+	flush_buffer();
+	if(selection == 'c')
+	{
+		return 1;
+	}
+	else
+	{
+		printf("Invalid input.\n");
+		return 1;
+	}
+}
+void loop(struct player players[], int num_players)
+{
+	for(int i=0; i<num_players; i++)
+	{
+		struct player curr_player=players[i];
+		printf("It is %s's turn.\n",curr_player.name);
+		struct turn trn;
+		trn.player=curr_player	;
+		trn.score=0;
+		for(int i=0; i<6; i++)
+			trn.dice[i]=0;
+		trn.num_remaining_dice=0;
+		for(;;)
+		{
+			char cont=execute_turn(trn);
+			if(!cont)
+				break;
+		}
+		
+	}
+}
+int main()
+{
+	//create some temp players.
+	struct player p1;
+	p1.name="Bob";
+	struct player p2;
+	p2.name="Joe";
+	struct player plrs[]={p1,p2};
+	loop(plrs, 2);
+	
+	return 0;
+}
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+gcc -o %1.exe %1.c
+%1.exe
